f2b4e44362e941b9d9ff83778395ebd6d9265e95
Виктория Семенова <vikster222@gmail.com>
2020-09-11 15:35:51 +0300

    2

--- Практика 1/01/Упражнение 2/Program.cs
+++ Практика 1/01/Упражнение 2/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 
-namespace upr2
+namespace CreateStruct
 {
+    public enum Genders : int { Male, Female };
+    class Person
+    {
+        public string firstName;
+        public string lastName;
+        public int age;
+        public Genders gender;
+        public Person(string _firstName, string _lastName, int _age, Genders _gender)
+        {
+            firstName = _firstName;
+            lastName = _lastName;
+            age = _age;
+            gender = _gender;
+        }
+        public override string ToString()
+        {
+            return firstName + " " + lastName + " (" + gender + "), age " + age;
+        }
+    } 
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person p = new Person("Tony", "Allen", 32, Genders.Male);
+            Console.WriteLine(p.ToString());
+
+            Console.ReadKey();
         }
     }
 }
